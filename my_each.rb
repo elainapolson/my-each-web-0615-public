c400d5f2b4f60ab32064cbbe82010c16e3af76a1
elainapolson <elainapolson@gmail.com>
2015-06-04 16:57:28 -0400

    done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  i = 0
+  length_of_array = words.length
+  loop do 
+    if i < length_of_array
+      yield words[i]
+      i += 1
+    else
+      break
+    end
+  end 
+  words
 end
